94502d356178472ea2106288d6945e7d823f27f4
김대훈 <eogns136@gmail.com>
2025-12-15 14:38:56 +0900

    step_1: ArrayList를 이용하여 데이터 삽입 완료

--- src/step1234/Main.java
+++ src/step1234/Main.java
@@ -1,7 +1,18 @@
 package step1234;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Product product = new Product("Galaxy S25", "1,200,000", "최신 안드로이드 스마트폰", 20);
+        product.setProductMembers("iPhone 16", "1,350,000", "Apple의 최신 스마트폰", 20);
+        product.setProductMembers("MacBook Pro", "2,400,000", "M3 칩셋이 탑재된 노트북", 20);
+        product.setProductMembers("AirPods Pro", "350,000", "노이즈 캔슬링 무선 이어폰", 20);
 
+        System.out.println(product.getProductMembers().get(2));
+        for (int i = 0; i < product.getProductMembers().size(); i++) {
+            System.out.println((i+1) + ". " +   product.getProductMembers().get(i));
+        }
     }
 }
--- src/step1234/Product.java
+++ src/step1234/Product.java
@@ -1,4 +1,33 @@
 package step1234;
 
+import java.util.ArrayList;
+
 public class Product {
+    String productName;  //상품 이름
+    String price;  //상품 가격
+    String description; //상품 설명
+    int stock;  //상품 재고
+
+    public Product(String productName, String price, String description, int stock) {
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+
+    ArrayList<Product> products = new ArrayList<>();
+
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductMembers(String productName, String price, String description, int stock) {
+        products.add(new Product(productName, price, description, stock));
+    }
+
+    public ArrayList<Product> getProductMembers() {
+        return products;
+    }
+
+
+
 }
