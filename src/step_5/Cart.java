c17012f01c17e5565959a0dd3e61665b3f3712b2
김대훈 <eogns136@gmail.com>
2025-12-17 17:18:25 +0900

    step_5_1: 주문 취소기능 구현(구현 완료)

--- src/step_5/Cart.java
+++ src/step_5/Cart.java
@@ -1,24 +1,25 @@
 package step_5;
 
+import java.util.ArrayList;
+
 // 장바구니 관리 클래스
 //  - addCartItem(): 상품 추가 시 재고 확인 및 중복 상품 수량 업데이트
 //  - cartIsEmpty(): 장바구니가 비어있는지 확인
 //  - showCart(): 장바구니 목록 및 총 금액 출력
 //  - getTotalAmount(): 총 금액 계산
 //  - completeOrder(): 주문 완료 시 재고 차감 및 장바구니 초기화
-
-import java.util.ArrayList;
-import java.util.Scanner;
+//  - cancelOrder(): 주문 취소 시 주문 했던 상품 재고량 복구
 
 public class Cart {
     ArrayList<CartItem> items = new ArrayList<>();  //리스트 객체 생성
+    ArrayList<CartItem> orderItems = new ArrayList<>();  //item.claer 연산으로 주문내역이 사라지므로 주문 내역을 저장하기 위한 리스트
 
-    public boolean addCartItem(Product product, int quantity) {  //상품 정보들과 수량을 매개변수로 장바구니 추가
+    public void addCartItem(Product product, int quantity) {  //상품 정보들과 수량을 매개변수로 장바구니 추가
         //남은 재고가 없는 경우
         if(product.getStock() < quantity) {
             System.out.println("상품 재고가 없습니다.");
             items.remove(product);
-            return false;
+            return;
         }
         //상품이 이미 있는 경우
         for (CartItem item : items) { //items의 상품을 반복하며 선택한 상품과 동일한지 확인
@@ -28,7 +29,7 @@ public class Cart {
                 item.setQuantity(item.getQuantity() + quantity);
                 product.setStock(product.getStock() - quantity); //주문한 수량만큼 재고 감소
                 System.out.println(product.getProductName() + "이(가) 장바구니에 더 추가되었습니다.");
-                return true;
+                return;
             }
         }
         //새 상품 추가
@@ -36,7 +37,6 @@ public class Cart {
         product.setStock(product.getStock() - quantity); //주문한 수량만큼 재고 감소
         System.out.println("현 재고" + product.getStock());
         System.out.println(product.getProductName() + "이(가) 장바구니에 추가되었습니다.");
-        return true;
     }
 
     public boolean cartIsEmpty() {  //장바구니가 비어있는지 확인
@@ -61,11 +61,38 @@ public class Cart {
     }
 
     public void completeOrder(){
+
+        orderItems.clear();
+        for (CartItem item : items) {
+            orderItems.add(new CartItem(item.getProduct(), item.getQuantity()));
+        }
+
         System.out.println("주문이 완료되었습니다! 총 금액: " + getTotalAmount());
         for (CartItem item : items) {
-            System.out.println(item.getProduct().getProductName() + " 재고가 " + item.getStockBeforeOrder() + "개 -> " + item.getProduct().getStock() + "개로 업데이트 되었습니다. \n");
+            System.out.println(item.getProduct().getProductName() + " 재고가 " + item.getStockBeforeOrder() + "개 -> " + item.getProduct().getStock() + "개로 업데이트 되었습니다.");
         }
 
         items.clear();  //장바구니 초기화
+    }
+
+    public void cancelOrder(){
+        if (orderItems.isEmpty()) {
+            System.out.println("주문내역이 없습니다");
+            return;
+        }
+        System.out.println("[ 주문 내역 ]");
+        for (int i = 0; i < orderItems.size(); i++) {
+            System.out.println(orderItems.get(i));
+        }
+
+        System.out.println("주문을 취소합니다.");
+        for (CartItem item : orderItems) {
+            Product product = item.getProduct();
+            int afterOrderStock = product.getStock();
+            product.setStock(afterOrderStock + item.getQuantity());   //주문으로 차감된 재고량의 주문 수량 더하기
+            System.out.println(product.getProductName() +" 재고가 " + afterOrderStock + "개 -> " +  product.getStock() + "개로 업데이트 되었습니다.");
+        }
+
+
     }
 }
--- src/step_5/CommerceSystem.java
+++ src/step_5/CommerceSystem.java
@@ -16,12 +16,11 @@ public class CommerceSystem {
                     System.out.println(i+1 + ". " + category.getCategoryName().get(i));
                 }
                 //장바구니 목록이 있다면, 주문관리 메뉴 추가
+                System.out.println("\n [ 주문 관리 ]");
                 if(!cart.cartIsEmpty()){
-                    System.out.println("\n [ 주문 관리 ]");
                     System.out.println("4. 장바구니 확인");
-                    System.out.println("5. 주문 취소");
                 }
-
+                System.out.println("5. 주문 취소");  //주문 취소 기능을 사용하기 위해 장바구니 목록이 없더라도 출력으로 변경
                 System.out.println("0. 종료       | 프로그램 종료");
                 menuNumber = sc.nextInt();
 
@@ -29,37 +28,52 @@ public class CommerceSystem {
                     case 0:
                         System.out.println("커머스 플랫폼을 종료합니다.");
                         System.exit(0);
-                    case 1:
+                    case 1:  //전자제품
                         category.categoryProducts(menuNumber);   //메뉴에 맞는 상품 출력
                         productNum = sc.nextInt();
                         category.choseProduct(productNum, cart);  //상품 선택 메서드
                         break;
-                    case 2:
+                    case 2:  //의류
                         category.categoryProducts(menuNumber);
                         productNum = sc.nextInt();
                         category.choseProduct(productNum, cart);
                         break;
-                    case 3:
+                    case 3:  //식품
                         category.categoryProducts(menuNumber);
                         productNum = sc.nextInt();
                         category.choseProduct(productNum, cart);
                         break;
-                    case 4:
-                        cart.showCart();
-                        System.out.println("\n[ 총 주문 금액 ]");
-                        System.out.println(cart.getTotalAmount() + "원 \n");
+                    case 4:  //장바구니 확인
+                        if(!cart.cartIsEmpty()){   //장바구니 내역이 없을 경우 오류 메시지 출력
+                            cart.showCart();
+                            System.out.println("\n[ 총 주문 금액 ]");
+                            System.out.println(cart.getTotalAmount() + "원 \n");
 
-                        System.out.println("1. 주문 확정     2. 메인으로 돌아가기");
-                        int input = sc.nextInt();
-                        if(input == 1){
-                            cart.completeOrder();
-                        } else if (input == 2) {
-                            System.out.println("메인화면으로 돌아갑니다.");
+                            System.out.println("1. 주문 확정     2. 메인으로 돌아가기");
+                            int input = sc.nextInt();
+                            if(input == 1){
+                                cart.completeOrder();
+                                break;
+                            } else if (input == 2) {
+                                System.out.println("메인화면으로 돌아갑니다.");
+                                break;
+                            }
+                        } else {
+                            System.out.println("해당 메뉴에 보이는 번호를 입력해주세요.");
+                        }
+                    case 5:  //주문 취소
+                        System.out.println("주문을 취소하시겠습니까?   1. 주문 취소     2. 메인으로 돌아가기");
+                        int cancelNum = sc.nextInt();
+                        if(cancelNum == 1){
+                            cart.cancelOrder();
                             break;
                         }
-
-//                case 5:
-//                    //장바구니 상품 들어있으면 주문 취소 기능
+                        else if(cancelNum == 2){
+                            System.out.println("메인 화면으로");
+                            break;
+                        }
+                    default:
+                        System.out.println("해당 메뉴에 보이는 번호를 입력해주세요.");
                 }
             }
         }catch (Exception e){
