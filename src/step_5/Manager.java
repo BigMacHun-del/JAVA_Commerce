f0f3376a34098f4a428ab2477f4ae4d88476e52c
김대훈 <eogns136@gmail.com>
2025-12-18 15:22:17 +0900

    step_5_2: 상품 삭제 기능 구현 완료

--- src/step_5/Manager.java
+++ src/step_5/Manager.java
@@ -33,15 +33,13 @@ public class Manager {
                         updateProduct(category);
                         break;
                     case 3:
-                        //상품 삭제
+                        removeProduct(category);
                         break;
                     case 4:
                         //전체 상품 현황
                         System.out.println("[ 전체 상품 현황 ]");
-//                for (int i = 0; i < category.getProducts().size(); i++) {
-//                    System.out.println(i+1 + ". " + category.getProducts().get(i));
-//                }
-                        /*TODO: Category 클래스에서는 되는데 같은 함수인데도 재고량 갱신이 안됨
+
+                        /*TODO: Category 클래스에서는 되는데 같은 함수인데도 재고량 갱신이 안됨 -> 해결완료
                         아마도 Category 클래스에서는 ArrayList로 받고 이 클래슨느 Category 그자체 객체에 담으니까 다른 객체로 인식하는 듯
                         --> 위 의심이 맞았고, 매개변수로 Category 객체를 받아 값을 전달해주었음.
                          */
@@ -67,15 +65,16 @@ public class Manager {
         }
         int categoryNum = sc.nextInt();
         sc.nextLine();
-        String selectcategoryName = category.getCategoryName().get(categoryNum - 1);
+        String selectCategoryName = category.getCategoryName().get(categoryNum - 1);
 
-        System.out.println("[ " +  selectcategoryName + "카테고리에 상품 추가 ]");
+        System.out.println("[ " +  selectCategoryName + "카테고리에 상품 추가 ]");
         System.out.print("상품명을 입력해주세요: ");
         String productName = sc.nextLine();
 
+        /// TODO: 검증 안됨
         //중복 상품명 검증(같은 카테고리 내에서)
         for (Product product : category.getProducts()) { //상품 클래스를 반복하며 현재 객체의 카테고리를 찾고, 상품 명 비교
-            if (productName.equals(selectcategoryName) &&  product.getProductName().equals(productName)) {
+            if (productName.equals(selectCategoryName) &&  product.getProductName().equals(productName)) {
                 System.out.println("이미 해당 카테고리에 동일한 이름의 상품이 있습니다");
                 return;
             }
@@ -144,6 +143,7 @@ public class Manager {
                 String beforePrice = selectProduct.getPrice();  //수정 전 값 저장
                 System.out.println("새로운 가격을 입력해주세요: ");
                 int afterPrice = sc.nextInt();
+                sc.nextLine();
                 String afterPriceStr = String.format("%,d", afterPrice);   //int를 String 000,000,000 형식 변환
 
                 selectProduct.setPrice(afterPriceStr);
@@ -163,6 +163,7 @@ public class Manager {
                 int beforeStock = selectProduct.getStock();
                 System.out.println("새로운 재고수량을 입력해주세요: ");
                 int afterStock = sc.nextInt();
+                sc.nextLine();
                 selectProduct.setStock(afterStock);
                 System.out.println(productName + "의 재고수량이 " + beforeStock + " -> " + afterStock + "으로 수정되었습니다.");
                 break;
@@ -170,8 +171,41 @@ public class Manager {
         }
     }
 
-    public void removeProduct(){
+    public void removeProduct(Category category) {
+        // 전체 보여주고, 상품명 입력받고, 해당 상품 삭제, 완료 메시지
+        System.out.println("아래 리스트를 보고, 삭제 할 상품의 상품명을 입력해주세요.");
+        for (int i=0; i<category.getProducts().size(); i++) {
+            System.out.println(i+1 + ". " + category.getProducts().get(i));
+        }
 
+        Product selectProduct = null;
+        boolean selectToken = false;   //상품을 찾았는지 못찾았는지 반환
+        String removeName = sc.nextLine();
+
+        for (Product product : category.getProducts()) {
+            if (removeName.equals(product.getProductName())) {
+                selectToken = true;
+                selectProduct = product;
+                System.out.println("삭제 할 상품 정보: " +  selectProduct + "\n");
+
+                System.out.println("정말 삭제 하시겠습니까? 1. 삭제    아무 키: 취소 ");
+                String choice = sc.nextLine();
+                if (choice.equals("1")) {
+                    category.getProducts().remove(selectProduct);
+                    System.out.println("정상적으로 삭제되었습니다.");
+                    break;
+                } else {
+                    System.out.println("삭제 취소");
+
+                    break;
+                }
+
+            }
+
+        }
+        if (!selectToken) {
+            System.out.println("입력하신 상품을 찾을 수 없습니다.");
+        }
     }
 
 
